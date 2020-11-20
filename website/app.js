fe76a42f00859d2173554298d72b14f8108f504f
basma123456 <72777076+basma123456@users.noreply.github.com>
2020-11-19 16:36:53 -0800

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,9 +1,60 @@
 ////////////variables/////////////////
 const api = '&appid=5910e41b3aa42587c5509f3e28fd0332';
-//const zip = document.getElementById('country').value;
-let zip = "85005";
 const myUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 /////////////////////////////////////////////////////////
+/* var d = new Date();
+
+var ourDateNow = d.getUTCFullYear() + " / " + d.getMonth() + " / " + d.getDate();
+console.log("llllllllllllllllllllllllllllllllllll" + ourDateNow); */
+
+//////////////////////////////////////////////////////////
+
+/*
+** the next code show addEventListener method that enable us to put an event and 
+** then a callback function which is executed when the event executrd
+** the callback function is called here performAction and has a parameter which is 
+** the zip code or the country code which user entered in the input
+*/
+
+document.getElementById('generate').addEventListener('click' , performAction);
+
+function performAction(e){
+    
+   const zip = document.getElementById('country').value;
+   if(zip.length > 0){
+    fav = document.getElementById('myText').value;
+    const d = new Date();
+    const ourMonth =  d.getMonth() + 1;
+
+     ourDateNow = d.getDate() + " / " + ourMonth  + " / " + d.getUTCFullYear()  ;
+
+
+
+    
+
+    getInfo(myUrl,zip,api).then(function(data){  
+                                                postData("/addCountry" , {temp:data.main.temp , fav:fav , ourDateNow : ourDateNow});
+                                                updateUi();
+                                                // notice when i put  updateUi() alone inside another .then()  
+                                                // it resulted in delay of the result so i needed to press two times to 
+                                                //release the right result 
+
+
+                                } ); 
+        }
+
+    }
+
+
+///////////////////////////////////////////////////////////
+
+/*
+** the function of post request which has 2 parameters
+** the first is the url of the distination that we want to post in it
+** the second parameter is the object that contains the data that we want to post it
+** and that function we must to make another function in the server.js which is
+** related to that post function and having the same url of that function
+*/
 
 const postData = async(url='' , data={})=>{
     const response = await fetch(url, {
@@ -20,10 +71,10 @@ const postData = async(url='' , data={})=>{
 
     try{
 
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-        res.send(newData);
+        const myData = await response.json();
+        console.log(myData);
+
+        return myData;
 
         
     }
@@ -35,18 +86,10 @@ const postData = async(url='' , data={})=>{
 }
 
 
-postData('/bonbon' , {'data' : 'is bonbon'});
 
 
+//////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////
-///////////////////////////////////////////////////////////
-
-document.getElementById('generate').addEventListener('click' , performAction);
-
-function performAction(e){
-    getInfo(myUrl,zip,api);
-}    
 
 
 const getInfo = async (myUrl,zip,api)=>{
@@ -54,9 +97,8 @@ const getInfo = async (myUrl,zip,api)=>{
     
 
     try{
-        const data = await res.json();
-        console.log(data);
-        res.send(data);
+         data = await res.json();
+
         return data;
     } catch(error){
 
@@ -66,33 +108,37 @@ const getInfo = async (myUrl,zip,api)=>{
 
 
 }
-const data = getInfo.data;
-console.log(data);
+//var data = getInfo.data;
+//console.log(data);
 
 ///////////////////////
 
-postData("/countryInfo" , data);
 
 
 
 ////////////////////////////////////////////////
-/*document.getElementById('generate').addEventListener('click' , performAction);
+///////////////////////////////////////////////
+/*
+** that function is enabling to show on the screen of the data that we post it and then get it
+*/
 
-function performAction(e){
-    getInfo("/country");
-}    
+ const updateUi = async () => {
 
 
-const getInfo = async (e)=>{
-    const res = await fetch(e);
-    
+    const req = await fetch("/all")
 
     try{
-        const data = await res.json();
-        console.log(data);
-        return data;
-        res.send(zip);
-        console.log(zip);
+
+
+         finalData = await req.json();
+
+        document.getElementById("showDate").innerHTML = "<b>Today is : </b>" + "<b>" + finalData[finalData.length-1].ourDateNow + "</b>";
+
+        document.getElementById("showTemp").innerHTML = "<b>The Temprature : </b>" + "<b>" +  finalData[finalData.length-1].temp + "</b>";
+        document.getElementById("showFeel").innerHTML = "<b>You Feel : </b>" + "<b>" +  finalData[finalData.length-1].fav + "</b>";
+
+
+
     } catch(error){
 
         console.log("error" , error);
@@ -100,6 +146,16 @@ const getInfo = async (e)=>{
     }
 
 
+
 }
+
+
+
+/*
+** first : we post the zip code by funcion of post function in app.js amd make another post function in the server.js and also make another get function in server.js by /all to hold the returned data
+** second : we post again the returned data but only temprature only in the /addCountry
+** third : we get the data again by the get function which  in the app.js which has await method 
+** fourth : we show the data on the screen by the updateUi function
 */
+
 
